b433607e8fb7f700a50019c14f94adec51e776d5
Eileen Li <eileenlimur@gmail.com>
2023-01-08 22:42:48 -0500

    Show first post on user/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @first_post = @user.microposts.first ? @user.microposts.first.content : nil
   end
 
   # GET /users/new
